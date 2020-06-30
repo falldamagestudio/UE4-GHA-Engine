c0c1c58d21ce95357762469b72d771723f811040
Mikael Kalms <mikael@kalms.org>
2020-06-30 23:19:30 +0200

    Minimal build script: Build engine, do not upload

---
+++ Scripts/Windows/BuildEngine.ps1
@@ -0,0 +1,4 @@
+
+cd ..\..\UE4
+
+. Engine\Build\BatchFiles\RunUAT.bat BuildGraph -Script="Engine/Build/InstalledEngineBuild.xml" -Target="Make Installed Build Win64" -set:HostPlatformOnly=true -set:WithServer=true -set:WithClient=true -set:WithDDC=false -set:VS2019=true
