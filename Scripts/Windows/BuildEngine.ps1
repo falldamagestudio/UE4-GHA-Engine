beff7b8e1d629514396eaa0a747da82ad8c01ced
Kalmalyzer <mikael@kalms.org>
2020-07-01 11:34:30 +0200

    Use script-relative path for UE4 folder

--- Scripts/Windows/BuildEngine.ps1
+++ Scripts/Windows/BuildEngine.ps1
@@ -1,4 +1,4 @@
 
-cd ..\..\UE4
+cd $PSScriptRoot\..\..\UE4
 
 . Engine\Build\BatchFiles\RunUAT.bat BuildGraph -Script="Engine/Build/InstalledEngineBuild.xml" -Target="Make Installed Build Win64" -set:HostPlatformOnly=true -set:WithServer=true -set:WithClient=true -set:WithDDC=false -set:VS2019=true
