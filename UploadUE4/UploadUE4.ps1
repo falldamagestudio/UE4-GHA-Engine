e7fd46623bea8a1ef3b6bfb61e954abae0496b1e
Mikael Kalms <mikael@kalms.org>
2020-07-24 20:31:49 +0200

    Do not load online configuration, it no longer exists

--- UploadUE4/UploadUE4.ps1
+++ UploadUE4/UploadUE4.ps1
@@ -9,9 +9,6 @@ $Here = $PSScriptRoot
 
 try {
 
-	$OnlineConfigurationLocation = Join-Path -Path $Here -ChildPath "online_configuration.json"
-	$OnlineConfiguration = Get-Content -Path $OnlineConfigurationLocation | ConvertFrom-Json
-
 	$LocalConfigurationLocation = Join-Path -Path $Here -ChildPath "local_configuration.json"
 	$LocalConfiguration = Get-Content -Path $LocalConfigurationLocation | ConvertFrom-Json
 
